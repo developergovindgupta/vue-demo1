e5be99cd00a6c2ef1d1d86411a85e9086296dbc3
developergovindgupta <govindprasadguptamca@gmail.com>
2022-01-23 22:55:46 +0530

    update

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,17 @@
-npm run build
+echo off
 cd dist
 git init
 git add -A
 git commit -m 'deploy'
 git push -f https://github.com/developergovindgupta/vue-demo1.git master:gh-pages
 cd ..
+echo.
+echo.
+echo. 
+echo doployment done on github.io
+echo.
+echo.
+echo ctrl+click view https://developergovindgupta.github.io/vue-demo1/
+echo.
+echo.
+echo on
--- gh-pages-deploy.js
+++ gh-pages-deploy.js
@@ -3,6 +3,7 @@ const execa = require('execa');
 const fs = require('fs');
 (async () => {
 	try {
+		// await execa('git', ['checkout', '--orphan', 'gh-pages']);
 		// eslint-disable-next-line no-console
 		console.log('Building started...');
 		await execa('npm', ['run', 'build']);
@@ -11,10 +12,10 @@ const fs = require('fs');
 		await execa('cd', [folderName]);
 		await execa('git', ['init']);
 		await execa('git', ['add', '-A']); //git add -A
-		await execa('git', ['commit', '-m', '"deploy-node"']); //git commit -m 'deploy'
+		await execa('git', ['commit', '-m', 'deploy']); //git commit -m 'deploy'
 		//git push -f https://github.com/developergovindgupta/vue-demo1.git master:gh-pages
 		await execa('git', ['push', '-f', 'https://github.com/developergovindgupta/vue-demo1.git', 'master:gh-pages']);
-		// await execa('cd', ['..']);
+		await execa('cd', ['..']);
 		console.log('Successfully deployed, check your settings');
 	} catch (e) {
 		// eslint-disable-next-line no-console
